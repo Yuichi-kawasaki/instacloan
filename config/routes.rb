702578ec44320d7fa4fce42e479454009e3054fa
yuichi kawasaki <jiv.yu1.kawasaki@gmail.com>
2020-08-11 12:47:24 +0900

    画像アップローダー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users do
+    collection do
+      post :confirm
+    end
+    collection do
+      get :star
+    end
+  end
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  resources :favorites, only: [:create, :destroy, :index]
 end
