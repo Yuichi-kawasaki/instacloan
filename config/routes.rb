03bf2c257fa5333f8fb7efbbdeddceee097912e6
yuichi kawasaki <jiv.yu1.kawasaki@gmail.com>
2020-08-11 16:35:05 +0900

    多対多

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  resources :favorites, only: [:create, :destroy, :index]
+  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  # resources :favorites, only: [:create, :destroy, :index]
 end
